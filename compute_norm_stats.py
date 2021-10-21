fb520d01b62b7f9fcc990c342634e95bcee1ac23
JamieChanCY <chanjamie43@yahoo.com>
2021-10-21 11:58:53 +0200

    put functions in stats_utils

--- compute_norm_stats.py
+++ compute_norm_stats.py
@@ -1,29 +1,23 @@
 """
 Using *only* the functions provided in this project, adjust this script wo it does the following:
 """
-
+import stats_utils as su
 # 1. generate two groups of 100 data points, one with a mean of 1 and the other with a mean of 1.4, both with sd  of 1.5
-from gen_data import generate_random_normal
-group1 = generate_random_normal(100, 1, 1.5)
-group2 = generate_random_normal(100, 1.4, 1.5)
+group1 = su.generate_random_normal(100, 1, 1.5)
+group2 = su.generate_random_normal(100, 1.4, 1.5)
 
 
 # 2. Can we tell that the two groups' populations have different means?
 # Print the t statistic and pvalue comparing the two groups
-from ttest import ttest_t_stat
-from ttest_pvalue import ttest_p_value
-t = ttest_t_stat(group1, group2)
-p = ttest_p_value(group1, group2)
+t = su.ttest_t_stat(group1, group2)
+p = su.ttest_p_value(group1, group2)
 print(t, p)
 
 # 3. Is the two groups' data correlated with each other?
 # print the spearman correlation statistic and pvalue between the two groups
-from correlation import correlation_statistic
-from correlation_pvalue import correlation_pvalue
-r = correlation_statistic(group1, group2, 'spearman')
-p = correlation_pvalue(group1, group2, 'spearman')
+r = su.correlation_statistic(group1, group2, 'spearman')
+p = su.correlation_pvalue(group1, group2, 'spearman')
 print(r, p)
 
 # 4. Plot the data
-from plots import scatter_plot
-scatter_plot(group1, group2)
+su.scatter_plot(group1, group2)
---
+++ stats_utils/__init__.py
@@ -0,0 +1,6 @@
+from .correlation import correlation_statistic
+from .correlation_pvalue import correlation_pvalue
+from .gen_data import generate_random_normal
+from .plots import scatter_plot
+from .ttest import ttest_t_stat
+from .ttest_pvalue import ttest_p_value
